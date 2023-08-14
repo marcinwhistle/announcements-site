ba3d50e726a5e7c164db33c1e8149cba6172400d
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-08-14 11:43:23 +0200

    Add mongoose and model for announcement

---
+++ models/announcent.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const announcementSchema = new mongoose.Schema({
+  _id: { type: mongoose.Types.ObjectId, required: true },
+  title: { type: String, required: true, minLength: 10, maxLength: 50 },
+  detail: { type: String, required: true, minLength: 20, maxLength: 1000 },
+  date: { type: Date, required: true },
+  photo: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  seller: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Announcement', announcementSchema);
