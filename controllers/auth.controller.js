27810034fef1f97f8f533adce6dae87d56fecd11
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-09-20 14:52:27 +0200

    Add block for different images type

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,30 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
+const getImageFileType = require('../utils/getImageFileType');
 
 exports.register = async (req, res) => {
   try {
-    const { login, password, avatar, phoneNumber } = req.body;
+    const { login, password, phoneNumber } = req.body;
+
+    const fileType = req.file ? await getImageFileType(req.file) : 'unknown';
+
     console.log(req.body, req.file);
+
     if (
       login &&
       typeof login === 'string' &&
       password &&
-      typeof password === 'string'
+      typeof password === 'string' &&
+      req.file &&
+      ['image/png', 'image/jpeg', 'image/gif'].includes(fileType)
     ) {
+      const [, ext] = req.file.originalname.split('.');
+      if (ext !== 'jpg' && ext !== 'png' && ext !== 'jpeg') {
+        res.status(400).send({ message: 'File ext is not correct' });
+      }
+
       const userWithLogin = await User.findOne({ login });
+
       if (userWithLogin) {
         return res
           .status(409)
@@ -24,6 +37,7 @@ exports.register = async (req, res) => {
         avatar: req.file.filename,
         phoneNumber,
       });
+
       res.status(201).send({ message: 'User created ' + user.login });
     } else {
       res.status(400).send({ message: 'Bad request' });
@@ -44,6 +58,7 @@ exports.login = async (req, res) => {
       typeof password === 'string'
     ) {
       const user = await User.findOne({ login });
+
       if (!user) {
         res.status(400).send({ message: 'Login or password are incorrect' });
       } else {
