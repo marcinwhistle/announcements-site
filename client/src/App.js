88cbec73b9516e76702c8f172239ae180cadc778
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-09-22 14:51:49 +0200

    Finish Routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,12 @@ import Home from './components/pages/Home/Home';
 import Ad from './components/pages/Ad/Ad';
 import AdAdd from './components/pages/AdAdd/AdAdd';
 import AdEdit from './components/pages/AdEdit/AdEdit';
+import AdRemove from './components/pages/AdRemove/AdRemove';
+import SearchPhrase from './components/pages/SearchPhrase/SearchPhrase';
+import Login from './components/pages/Login/Login';
+import Register from './components/pages/Register/Register';
+import Logout from './components/pages/Logout/Logout';
+import NotFound from './components/pages/NotFound/NotFound';
 
 const App = () => {
   return (
@@ -16,6 +22,12 @@ const App = () => {
           <Route path='/ad/:id' element={<Ad />} />
           <Route path='/ad/add' element={<AdAdd />} />
           <Route path='/ad/edit/:id' element={<AdEdit />} />
+          <Route path='/ad/remove/id' element={<AdRemove />} />
+          <Route path='/search/:searchPhrase' element={<SearchPhrase />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Provider>
     </React.StrictMode>
---
+++ client/src/components/pages/SearchPhrase/SearchPhrase.js
@@ -0,0 +1,9 @@
+const SearchPhrase = () => {
+  return (
+    <div>
+      <h1>SearchPhrase</h1>
+    </div>
+  );
+};
+
+export default SearchPhrase;
